ab269294c57bc5d51a9f66cdf502e190120c49f1
arindampattanayak <arindampattanayak2004@gmail.com>
2025-07-10 03:42:51 +0530

    updated code

--- MYSQL_CODES/UPDATE_OPERATION/update_final_balance.sql
+++ MYSQL_CODES/UPDATE_OPERATION/update_final_balance.sql
@@ -1,4 +1,10 @@
 
+
+
+
 UPDATE retailers r
 JOIN payments p ON r.retailer_id = p.retailer_id
-SET r.final_balance = p.final_balance;
+SET r.final_balance = p.final_balance
+WHERE r.retailer_id IN (
+    SELECT retailer_id FROM payments
+);
